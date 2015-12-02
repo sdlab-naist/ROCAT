3a21229b58a61269862c42ee67df6bf9a2be9832
Kevin Saito <yum02.skullcandy@gmail.com>
2015-12-03 00:42:38 +0900

    trim space and remove unuse code

--- Assets/CameraMove.cs
+++ Assets/CameraMove.cs
@@ -27,12 +27,11 @@ public class CameraMove : MonoBehaviour {
 		cc = GameObject.Find ("CityCreater").GetComponent<CityCreater> ();
 		foreach( Transform child in canvas.transform){
 			file_name = child.gameObject.GetComponent<Text>();
-//			file_name = GameObject.Find("Text");
 			file_name.text = "";
 		}
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
@@ -40,21 +39,19 @@ public class CameraMove : MonoBehaviour {
 			GetComponent<Rigidbody>().velocity = transform.forward * 100.0f;
 		}
 		if(Input.GetKey(KeyCode.LeftArrow)){
-			//GetComponent<Rigidbody>().velocity = transform.right *  -100.0f;
 			transform.Rotate(new Vector3(0,-0.8f,0));
 		}
-		if(Input.GetKey(KeyCode.DownArrow)){ 
+		if(Input.GetKey(KeyCode.DownArrow)){
 			GetComponent<Rigidbody>().velocity = transform.forward * -100.0f;
 		}
 		if(Input.GetKey(KeyCode.RightArrow)){
-			//GetComponent<Rigidbody>().velocity = transform.right * 100.0f;
 			transform.Rotate(new Vector3(0,0.8f,0));
 		}
 		if (Input.GetKey (KeyCode.Space) && Input.GetKey (KeyCode.LeftShift)) {
-			GetComponent<Rigidbody> ().velocity = transform.up * -100.0f; 
+			GetComponent<Rigidbody> ().velocity = transform.up * -100.0f;
 		}
 		else if (Input.GetKey (KeyCode.Space)) {
-			GetComponent<Rigidbody>().velocity = transform.up * 100.0f; 
+			GetComponent<Rigidbody>().velocity = transform.up * 100.0f;
 		}
 		if (Input.GetKey (KeyCode.V)) {
 			view_src = true;
@@ -90,11 +87,8 @@ public class CameraMove : MonoBehaviour {
 			}
 			file_name.text = "";
 		}
-		
+	}
 
-//		this.transform.Translate ( 0, 0,( Input.GetAxis ( "Vertical" ) * 1 ) );
-//		this.transform.Rotate (0,( Input.GetAxis ("Horizontal" )  * 1 ),0);
-	} 
 	void OnGUI()
 	{
 		if(view_src)
@@ -102,26 +96,14 @@ public class CameraMove : MonoBehaviour {
 			src_txt = GUI.TextArea (new Rect (5, 5, Screen.width-10, Screen.height-100), src_txt.Substring(0,Math.Min(1000,src_txt.Length)));
 
 	}
-	/*
-	void OnGUI () {
-		// ラベルを表示する
-		GUI.Label(new Rect(10,10,100,100), “MenuWindow”);
-	}
-	*/
 
-	void OnCollisionEnter(Collision collision){
-		 //file_name.text = collision.transform.name;
-	}
-	void OnCollisionExit(Collision collision){
-		//file_name.text = "";
-	}
 	string ReadFile(string path){
 		string st = "";
 		try {
 		// FileReadTest.txtファイルを読み込む
 		Debug.Log (path);
 		FileInfo fi = new FileInfo(path);
-		
+
 
 			// 一行毎読み込み
 			using (StreamReader sr = new StreamReader(fi.OpenRead(), Encoding.UTF8)){
@@ -138,7 +120,7 @@ public class CameraMove : MonoBehaviour {
 	string SearchPathFromFileName(string file_name){
 		string path = "";
 		IList buildings = cc.GetCity()["buildings"] as IList;
-		foreach (Dictionary<string,object> building in buildings) {	
+		foreach (Dictionary<string,object> building in buildings) {
 			if(building["name"].ToString() == file_name){
 				path = building["path"].ToString();
 			}
@@ -146,7 +128,7 @@ public class CameraMove : MonoBehaviour {
 		return path;
 
 	}
-	
+
 	// 改行コード処理
 	string SetDefaultText(){
 		return "cant read\n";
